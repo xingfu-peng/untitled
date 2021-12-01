5a8d1fa908238e4f69e22528a215f3b5faa3602d
xingfu.peng <xingfu.peng@capgemini.com>
2021-12-01 23:09:53 +0800

    实体类添加

---
+++ src/com/company/bean/person.java
@@ -0,0 +1,33 @@
+package com.company.bean;
+
+import java.util.List;
+
+public class person {
+    private String name;
+    private int age;
+    private List<String> likes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<String> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(List<String> likes) {
+        this.likes = likes;
+    }
+}
