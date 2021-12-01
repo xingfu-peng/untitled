5842831a9d0d3b7c5f03fb660486c66c95b27647
xingfu.peng <xingfu.peng@capgemini.com>
2021-12-01 23:08:30 +0800

    实体类创建

---
+++ src/com/company/bean/activity.java
@@ -0,0 +1,22 @@
+package com.company.bean;
+
+public class activity {
+    private int count;
+    private String name;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
